dd4deacd3c9f89c22684e5b55f61a98c27e09649
balaji <balajikandavel@gmail.com>
2014-10-21 18:48:23 -0400

    new user Exp

--- js/MadlibController.js
+++ js/MadlibController.js
@@ -2,6 +2,42 @@
 
 angular.module('madlib', []).controller('madlibController' ,function($scope){
 
+$scope.displayForm=true;
+
+$scope.checkSomething= function(){
+
+	return $scope.displayForm;
+
+}
+
+$scope.ShowAlternate = function(isValid){
+
+$scope.submitted=true;
+
+if(isValid)	{
+	$scope.displayForm= !$scope.displayForm;
+	}
+}
+
+$scope.clearData = function(ValidForm){
+
+$scope.ShowAlternate(ValidForm);
+$scope.female_name="";              
+$scope.job_title="";              
+$scope.tedious_task ="";              
+$scope.dirty_task="";              
+$scope.celebrity="";              
+$scope.useless_skill="";
+$scope.adjective="";
+$scope.obnoxiuous_celbertity="";
+$scope.huge_number="";
+}
+
+
+$scope.submitForm = function(){
+
+$scope.submitted=true;
+}
 
 
 });
